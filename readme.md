694d5d05262a1b0416b92442871ac62e63976318
fred-fittywebdev <frederic-guerra_student2021@wild.school>
2021-04-21 13:33:46 +0200

    Création de la bdd, des entités, mise en place du readme et création du HomeController

---
+++ readme.md
@@ -0,0 +1,17 @@
+# blogIt
+
+BlogIt est un petit blog réaliser pour apprendre et consolider les bases du framework Symfony 5
+Pas de joli front ou de fonctionnalités avancées, juste de quoi pratiquer un peu.
+
+## Pré-requis
+
+- PHP 7.4
+- Composer
+- Symfony 5
+- Symfony CLI
+
+Vous pouvez vérifier les pré-requis en tapant cette commande ( dans la cli de symfony)
+
+```bash
+symfony check:requirements
+```
---
+++ src/Entity/Commentaire.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentaireRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentaireRepository::class)
+ */
+class Commentaire
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datePost;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getDatePost(): ?\DateTimeInterface
+    {
+        return $this->datePost;
+    }
+
+    public function setDatePost(\DateTimeInterface $datePost): self
+    {
+        $this->datePost = $datePost;
+
+        return $this;
+    }
+}
