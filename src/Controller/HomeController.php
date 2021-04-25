7e5258f8062dc79b8f23448022e9faacd5731253
fred-fittywebdev <frederic-guerra_student2021@wild.school>
2021-04-25 17:53:23 +0200

    Ajout de la navbar et du footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,8 @@ use DateTimeInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
+use Dzango\Twig\Extension\Truncate;
+use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\TwigEnvironmentPass;
 
 class HomeController extends AbstractController
 {
